f4cbac254e07bb3870a826ef04bdf812d18cca95
Paul Rougieux <paul.rougieux@gmail.com>
2017-09-28 18:43:38 +0200

    create the cleancomextmonthly1product function, start by loading archive and recent data, with units

--- R/cleancomext.R
+++ R/cleancomext.R
@@ -26,3 +26,63 @@ extractpricew <- function(dtf, lowercoef= 0.5, uppercoef=2,
                   averagepricew = mean(pricew),
                   weightedaveragepricew = sum(tradevalue)/ sum(weight))
 }
+
+
+#' Clean Comext Monthly data
+#'
+#' @param RMySQLcon database connection object created by RMySQL \code{\link[DBI]{dbConnect}}
+#' @param tablearchive character name of a monthly archive table
+#' @param tablerecent character name of a monthly recent table
+#' @param tableprice character name of a price table
+#' @param tablecv character name of a conversion factor table
+#' @examples \dontrun{ # Clean product and country codes
+#' # Connect to the database
+#' con <- RMySQL::dbConnect(RMySQL::MySQL(), dbname = "test")
+#' # Clean product 44079910
+#'
+#' # Disconnect from the database
+#' RMySQL::dbDisconnect(con)
+#' }
+#' @export
+cleancomextmonthly1product <- function(RMySQLcon,
+                                       tablearchive,
+                                       tablerecent,
+                                       tableprice,
+                                       tablecv){
+    # load trade flows from the database into a data frame
+    # Load archive data
+    dtfa <- tbl(RMySQLcon, tablearchive) %>%
+        filter(productcode == productanalysed) %>%
+        # Add quantity units
+        eutradeflows::addunit2tbl(RMySQLcon,
+                                  maintbl = .,
+                                  tableunit = "vld_comext_unit")  %>%
+        collect()
+
+    # Load recent data
+    dtfr <- tbl(RMySQLcon, tablerecent) %>%
+        filter(productcode == productanalysed) %>%
+        # Add quantity units
+        eutradeflows::addunit2tbl(RMySQLcon,
+                                  maintbl = .,
+                                  tableunit = "vld_comext_unit")  %>%
+        collect()
+
+    # Combine archive and recent data
+    dtf <- rbind(dtfa, dtfr)
+
+    # Remove unnecessary objects
+    rm(dtfa)
+    rm(dtfr)
+
+    # Prepare conversion factors and prices,
+    # compute median prices, price bounds and conversion factors in a data frame
+    price <- tradeflows::extractprices(dtf, grouping = c("flow", "year", "unit"))
+    conversionfactor <- tradeflows::extractconversionfactors(dtf)
+
+    # calculate missing quantities
+    # shave prices
+    # store prices and conversion factors in the database
+    # store modified trade flows into the database
+
+}
